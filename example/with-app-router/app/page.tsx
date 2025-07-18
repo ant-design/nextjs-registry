81acd564ca1957f55d22e93fd9536c6995024a44
MadCcc <madccc@foxmail.com>
2025-07-18 18:02:46 +0800

    fix: style missing with loading

--- example/with-app-router/app/page.tsx
+++ example/with-app-router/app/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
-import React from 'react';
-import { Button, Checkbox, ConfigProvider, Form, Input } from 'antd';
+import React, { Suspense } from 'react';
+import { Button, Checkbox, ConfigProvider, Form, Input, Select } from 'antd';
 
 const onFinish = (values: any) => {
   console.log('Success:', values);
@@ -17,49 +17,64 @@ type FieldType = {
   remember?: string;
 };
 
-const Page: React.FC = () => (
-  <ConfigProvider theme={{ cssVar: true }}>
-    <Form
-      name="basic"
-      labelCol={{ span: 8 }}
-      wrapperCol={{ span: 16 }}
-      style={{ maxWidth: 600 }}
-      initialValues={{ remember: true }}
-      onFinish={onFinish}
-      onFinishFailed={onFinishFailed}
-      autoComplete="off"
-    >
-      <Form.Item<FieldType>
-        label="Username"
-        name="username"
-        rules={[{ required: true, message: 'Please input your username!' }]}
-      >
-        <Input />
-      </Form.Item>
+let promise;
+function genPromise() {
+  if (promise) return promise;
+  promise = new Promise((resolve) => {
+    setTimeout(() => {
+      resolve({});
+    }, 3 * 1000);
+  });
+  return promise;
+}
 
-      <Form.Item<FieldType>
-        label="Password"
-        name="password"
-        rules={[{ required: true, message: 'Please input your password!' }]}
-      >
-        <Input.Password />
-      </Form.Item>
+const Page: React.FC = () => {
+  React.use(genPromise());
 
-      <Form.Item<FieldType>
-        name="remember"
-        valuePropName="checked"
-        wrapperCol={{ offset: 8, span: 16 }}
+  return (
+    <ConfigProvider theme={{ cssVar: true }}>
+      <Form
+        name="basic"
+        labelCol={{ span: 8 }}
+        wrapperCol={{ span: 16 }}
+        style={{ maxWidth: 600 }}
+        initialValues={{ remember: true }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+        autoComplete="off"
       >
-        <Checkbox>Remember me</Checkbox>
-      </Form.Item>
+        <Form.Item<FieldType>
+          label="Username"
+          name="username"
+          rules={[{ required: true, message: 'Please input your username!' }]}
+        >
+          <Input />
+        </Form.Item>
 
-      <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-        <Button type="primary" htmlType="submit">
-          Submit
-        </Button>
-      </Form.Item>
-    </Form>
-  </ConfigProvider>
-);
+        <Form.Item<FieldType>
+          label="Password"
+          name="password"
+          rules={[{ required: true, message: 'Please input your password!' }]}
+        >
+          <Input.Password />
+        </Form.Item>
+
+        <Form.Item<FieldType>
+          name="remember"
+          valuePropName="checked"
+          wrapperCol={{ offset: 8, span: 16 }}
+        >
+          <Checkbox>Remember me</Checkbox>
+        </Form.Item>
+
+        <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+          <Button type="primary" htmlType="submit">
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
+    </ConfigProvider>
+  );
+};
 
 export default Page;
--- src/AntdRegistry.tsx
+++ src/AntdRegistry.tsx
@@ -1,23 +1,17 @@
 'use client';
 
-import React, { type FC, useRef, useState } from 'react';
-import { createCache, extractStyle, StyleProvider } from '@ant-design/cssinjs';
+import React, { type FC, useState } from 'react';
 import type { StyleProviderProps } from '@ant-design/cssinjs';
+import { createCache, extractStyle, StyleProvider } from '@ant-design/cssinjs';
 import { useServerInsertedHTML } from 'next/navigation';
 
 type AntdRegistryProps = Omit<StyleProviderProps, 'cache'>;
 
 const AntdRegistry: FC<AntdRegistryProps> = (props) => {
   const [cache] = useState(() => createCache());
-  const inserted = useRef(false);
 
   useServerInsertedHTML(() => {
-    const styleText = extractStyle(cache, { plain: true });
-
-    if (inserted.current) {
-      return null;
-    }
-    inserted.current = true;
+    const styleText = extractStyle(cache, { plain: true, once: true });
 
     return (
       <style
